09f9b57437d22b531f1bc8eed6320667b7d134ae
Ilyas Maazib <ilyasmaazib9@gmail.com>
2025-05-20 15:58:28 +0700

    first commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -157,7 +157,8 @@
         </main>
         <footer></footer>
     </div>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3/dist/cdn.min.js"></script>
+
 </body>
 
 </html>
